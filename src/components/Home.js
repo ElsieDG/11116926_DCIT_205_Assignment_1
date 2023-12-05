5561545c66b8b382da595d2fbf960693eaaaba37
Elsie D <elsiedegraftowoo@gmail.com>
2023-12-05 02:38:11 +0000

    Added home page and its styles

---
+++ src/components/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className='main-body home'>
+      <h1 className='home-heading'>Welcome to Student Grade System Services </h1><br></br>
+      <ul className='login-links'>
+        <li className='login-links b'><Link to="/form">Login</Link></li>
+        <li className='login-links b'><Link to="/contact">Contact the Support Desk</Link></li>
+        <li className='login-links b'><Link to="/about">FAQs</Link></li>
+      </ul> 
+      
+
+      <div className='overview'>
+        <h2 className='home-heading'>OVERVIEW</h2>
+        <p>Use our missing grade system to report unrecorded grades</p>
+        <p>View current and past semester's course grades</p>
+        <p>Find or contact your instructors</p>
+        <p>Get help on all your Academic record related issues</p>
+      </div>
+      
+    </div>
+
+  
+  );
+};
+
+export default Home;
