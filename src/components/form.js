c2f8c3d4f417bfd233ac6cbbe2cc811ac542ea67
Elsie D <elsiedegraftowoo@gmail.com>
2023-12-05 02:32:03 +0000

    updated login form and styles

--- src/components/form.js
+++ src/components/form.js
@@ -1,28 +1,25 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import './form.css';
 
-const MissingGradeForm = () => {
+const LoginForm = () => {
   return (
     <div className="container mt-5">
-      <h2 className="mb-4">Report Missing Grades</h2>
+      <h2 className="mb-4">Login</h2>
       <form>
         <div className="mb-3">
-          <label htmlFor="studentName" className="form-label">Student Name</label>
+          <label htmlFor="studentName" className="form-label">Student ID</label>
           <input type="text" className="form-control" id="studentName" />
         </div>
         <div className="mb-3">
-          <label htmlFor="course" className="form-label">Course</label>
-          <input type="text" className="form-control" id="course" />
+          <label htmlFor="course" className="form-label">Password</label>
+          <input type="password" className="form-control" id="course" />
         </div>
-        <div className="mb-3">
-          <label htmlFor="missingGrade" className="form-label">Missing Grade</label>
-          <input type="text" className="form-control" id="missingGrade" />
-        </div>
+    
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     </div>
   );
 }
 
-export default MissingGradeForm;
+export default LoginForm;
