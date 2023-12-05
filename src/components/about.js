fbe8a219ee2fa9cd963aefe16a9188d0713b7fe0
Elsie D <elsiedegraftowoo@gmail.com>
2023-12-05 02:34:03 +0000

    added about page

---
+++ src/components/about.js
@@ -0,0 +1,13 @@
+// src/components/About.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='main-body'>
+      <h1>About Us</h1>
+      <p>This is the about page content.</p>
+    </div>
+  );
+};
+
+export default About;
