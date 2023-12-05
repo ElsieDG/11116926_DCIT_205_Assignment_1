33c4cfbc7baf5edbd6de8aa21cd26c44a495c6c8
Elsie D <elsiedegraftowoo@gmail.com>
2023-12-05 02:33:26 +0000

    Updated app.js and its styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import MissingGradeForm from './components/form';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar';
+import Home from './components/Home';
+import About from './components/about';
+import Contact from './components/contact';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <MissingGradeForm />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+       <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/form" element={<MissingGradeForm />} />
+        </Routes>
+      </div>
+
+    </Router>
   );
 }
 
