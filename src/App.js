0aa7f70f71fcd0c3c5eca82c5d6872bcb002aab4
Elsie D <elsiedegraftowoo@gmail.com>
2023-12-13 04:11:40 +0000

    updated apps.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import MissingGradeForm from './components/form';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/navbar';
-import Home from './components/Home';
+import Navbar from './components/Navbar/navbar'
+import Footer from './components/Footer/Footer'
+import MissingGradeForm from './components/Login/form';
+import AuthenticationPage from './components/Login/Authentication';
+import Home from './components/Home/Home';
 import About from './components/about';
 import Contact from './components/contact';
+import Dashboard from './components/Dashboard/Dashboard';
+
 import './App.css';
 
 function App() {
   return (
+ <div className="App">
     <Router>
-      <div>
+      <div className="app-container">
         <Navbar />
        <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/form" element={<MissingGradeForm />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
         </Routes>
       </div>
-
+      
+      <Footer /> {/* Include the Footer component */}
     </Router>
+    </div>
   );
 }
 
